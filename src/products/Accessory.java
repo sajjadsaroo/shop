87c7f3c65709b30aa7af09a51b5ff601201fd6fa
Macsaroo <sajjad.sarookhani@gmail.com>
2025-04-30 00:27:47 +0330

    add Accessory class

---
+++ src/products/Accessory.java
@@ -0,0 +1,33 @@
+package products;
+
+public class Accessory extends Product{
+    private String color;
+    private static int counter = 1;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Accessory(String color , String title, double price) {
+        super(title, price);
+        setColor(color);
+    }
+
+    @Override
+    public String toString() {
+        String baseString = super.toString();
+        return baseString + " ,Color=" + getColor();
+    }
+
+    @Override
+    protected String generateId() {
+        if (counter > 999)
+            throw new IllegalStateException("Book ID limit reached");
+        return "3" + String.format("%03d", counter++);
+    }
+
+}
